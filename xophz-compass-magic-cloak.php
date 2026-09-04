8b763fcf0649236f6981c14d2a37cc8350dfe9c8
XP <x@hallofthegods.com>
2026-09-03 19:57:56 -0700

    refactor: inherit from Xophz_Compass_Plugin_Base and remove redundant loader, i18n, and deactivator classes

--- xophz-compass-magic-cloak.php
+++ xophz-compass-magic-cloak.php
@@ -54,8 +54,7 @@ function activate_xophz_compass_magic_cloak() {
  * This action is documented in includes/class-xophz-compass-magic-cloak-deactivator.php
  */
 function deactivate_xophz_compass_magic_cloak() {
-	require_once plugin_dir_path( __FILE__ ) . 'includes/class-xophz-compass-magic-cloak-deactivator.php';
-	Xophz_Compass_Magic_Cloak_Deactivator::deactivate();
+	Xophz_Compass_Magic_Cloak::deactivate();
 }
 
 register_activation_hook( __FILE__, 'activate_xophz_compass_magic_cloak' );
